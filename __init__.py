74626997f3be5f8a0b75b961f0b576516d4ff49f
namicad <namicad@gmail.com>
2022-11-01 19:22:50 +0900

    .

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    @app.route('/')
+    def home():
+#        return render_template('home.html')
+        return 'hello, pybo!'
+    
+    if __name__=='__main__':
+        app.run(debug=True)
 
-@app.route('/')
-def home():
-    return render_template('template/home.html')
-
-if __name__=='__main__':
-    app.run(debug=True)
+    return app
