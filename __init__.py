a4a7c998b35807495c4030df14c0a103fa30453e
namicad <namicad@gmail.com>
2022-11-01 23:22:13 +0900

    .

--- __init__.py
+++ __init__.py
@@ -1,12 +1,23 @@
 from flask import Flask
+import pymysql
 
 app = Flask(__name__)
 
-from .views import main_views, question_views, answer_views, auth_views
+from views import main_views, question_views, answer_views, auth_views
 
 app.register_blueprint(main_views.bp)
 # app.register_blueprint(question_views.bp)
 # app.register_blueprint(answer_views.bp)
 # app.register_blueprint(auth_views.bp)
+
+conn = pymysql.connect(host='localhost', user='root', password='password', charset='utf8')
+cursor = conn.cursor() 
+
+sql = "CREATE DATABASE developer" 
+
+cursor.execute(sql) 
+
+conn.commit() 
+conn.close() 
 
 app.run(debug=True)
