2ecf1d3c89d715b6623f14713eb457142c1cfe40
namicad <54072458+namicad@users.noreply.github.com>
2022-11-16 19:53:12 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 
-app = Flask(__name__)
-app.secret_key = "secret"
+def func():
+  app = Flask(__name__)
+  app.secret_key = "secret"
 
-from views import main_views, post_views, auth_views
+  from views import main_views, post_views, auth_views
 
-app.register_blueprint(main_views.bp)
-app.register_blueprint(post_views.bp)
-app.register_blueprint(auth_views.bp)
-
-app.run(debug=True)
+  app.register_blueprint(main_views.bp)
+  app.register_blueprint(post_views.bp)
+  app.register_blueprint(auth_views.bp)
+  
+  return app
